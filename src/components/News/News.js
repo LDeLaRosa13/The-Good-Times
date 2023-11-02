e924ea829d530a48b7044b0403add7eb98008c2e
Lauren DeLaRosa <lstoneman13@gmail.com>
2023-11-02 12:13:57 -0400

    Add urlToImage to Link to display image with article title

--- src/components/News/News.js
+++ src/components/News/News.js
@@ -3,7 +3,7 @@ import './News.css'
 const News = (props) => {
 return (
   <div className='news'>
-    <p>{props.title}</p>
+    {/* <p>{props.title}</p> */}
     <p>{props.description}</p>
   </div>
 )
